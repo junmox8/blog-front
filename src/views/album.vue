4d475221a9a5a8a70a98fddfd4d3c06428ccd25d
junmox8 <931097192@qq.com>
2022-09-27 19:05:51 +0800

    09-27-1

--- src/views/album.vue
+++ src/views/album.vue
@@ -1,9 +1,87 @@
 <template>
-  <div class="container"></div>
+  <el-dialog
+    v-model="dialogVisible"
+    title="添加分类"
+    width="30%"
+    :before-close="handleClose"
+  >
+    <span>This is a message</span>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">取消</el-button>
+        <el-button type="primary" @click="dialogVisible = false"
+          >确认</el-button
+        >
+      </span>
+    </template>
+  </el-dialog>
+  <div class="container">
+    <el-card class="album-container">
+      <el-tabs
+        v-model="editableTabsValue"
+        :tab-position="'left'"
+        type="card"
+        editable
+        stretch
+        class="demo-tabs"
+        @edit="handleTabsEdit"
+      >
+        <el-tab-pane
+          v-for="item in editableTabs"
+          :key="item.name"
+          :label="item.title"
+          :name="item.name"
+          >User</el-tab-pane
+        >
+      </el-tabs>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+export default {
+  setup() {
+    const editableTabsValue = ref("0");
+    const editableTabs = ref([]);
+    const dialogVisible = ref(false);
+    const handleTabsEdit = (targetName, action) => {
+      if (action === "add") {
+        dialogVisible.value = true;
+        const newTabName = "asd";
+        editableTabs.value.push({
+          title: "New Tab",
+          name: newTabName,
+          content: "New Tab content",
+        });
+        editableTabsValue.value = newTabName;
+      } else if (action === "remove") {
+        const tabs = editableTabs.value;
+        let activeName = editableTabsValue.value;
+        if (activeName === targetName) {
+          tabs.forEach((tab, index) => {
+            if (tab.name === targetName) {
+              const nextTab = tabs[index + 1] || tabs[index - 1];
+              if (nextTab) {
+                activeName = nextTab.name;
+              }
+            }
+          });
+        }
+
+        editableTabsValue.value = activeName;
+        editableTabs.value = tabs.filter((tab) => tab.name !== targetName);
+      }
+    };
+    return {
+      editableTabsValue,
+      editableTabs,
+      dialogVisible,
+      handleTabsEdit,
+      handleTabsEdit,
+    };
+  },
+};
 </script>
 
 <style scoped>
@@ -14,5 +92,26 @@ export default {};
   height: auto;
   background-image: url("https://huangjunyi-1310688513.cos.ap-shanghai.myqcloud.com/articleCover/%E6%91%84%E5%9B%BE%E7%BD%91_401729159_%E6%B8%90%E5%8F%98%E4%BD%8E%E5%A4%9A%E8%BE%B9%E5%BD%A2%E8%83%8C%E6%99%AF%EF%BC%88%E9%9D%9E%E4%BC%81%E4%B8%9A%E5%95%86%E7%94%A8%EF%BC%89.jpg");
   background-size: cover;
+  overflow: hidden;
+}
+.album-container {
+  width: 80%;
+  margin-top: 59px;
+  margin-left: 10%;
+  margin-bottom: 100px;
+  min-height: 600px;
+  height: auto;
+  animation-name: container;
+  animation-duration: 2s;
+}
+@keyframes container {
+  from {
+    opacity: 0;
+    transform: translateY(-100px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0px);
+  }
 }
 </style>
